f5c087678d97b9e8fc5ad039bad427d79ded4543
tomartin <tomartin@strudent.42madrid.org>
2024-04-28 23:13:11 +0200

    init check input file

--- src/check_file.c
+++ src/check_file.c
@@ -1,7 +1,13 @@
 #include "../includes/woody.h"
+#include <fcntl.h>
 #include <stdio.h>
 
 int check_the_origin_file(char *file) {
-  printf("FILE= %s", file);
+  int fd;
+  fd = open(file, O_RDONLY);
+  if (fd == -1) {
+    perror("Can not open the source file");
+    return (1);
+  }
   return (0);
 }
