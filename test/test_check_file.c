012ac4433b707b3a0eb46c1bf2853ccc20d9f1ad
tomartin <tomartin@strudent.42madrid.org>
2024-04-30 06:39:35 +0200

    test unity work

--- test/test_check_file.c
+++ test/test_check_file.c
@@ -1,19 +1,18 @@
 #include "../Unity/src/unity.h"
 #include "../includes/woody.h"
 #include <fcntl.h>
-#include <stdio.h>
 
-void test_check_the_origin_file(void);
+void setUp(void) {
+  // set stuff up here
+}
+
+void tearDown(void) {
+  // clean stuff up here
+}
+
+int check_the_origin_file(char *file);
 
 void test_check_the_origin_file(void) {
   TEST_ASSERT_EQUAL_INT(0, check_the_origin_file("hola"));
   TEST_ASSERT_EQUAL_INT(1, check_the_origin_file("xxxxx"));
-}
-
-int main(void) {
-  UNITY_BEGIN();
-
-  RUN_TEST(test_check_the_origin_file);
-
-  return UNITY_END();
 }
