b878b0d91f4c01bc00c6d0be0b000b7e77046ce8
tomartin <tomartin@strudent.42madrid.org>
2024-05-04 19:39:42 +0200

    understanding elf64 header file

--- src/check_file.c
+++ src/check_file.c
@@ -13,6 +13,29 @@ int check_the_origin_file(char *file) {
   }
   return (fd);
 }
+
+/*
+    unsigned char e_ident[EI_NIDENT]; Identificación de archivo ELF
+    Elf64_Half    e_type;             Tipo de archivo
+    Elf64_Half    e_machine;          Arquitectura del procesador
+    Elf64_Word    e_version;          Versión del archivo ELF
+    Elf64_Addr    e_entry;            Punto de entrada del programa
+    Elf64_Off     e_phoff;            Desplazamiento del encabezado del programa
+    Elf64_Off     e_shoff;            Desplazamiento del encabezado de sección
+    Elf64_Word    e_flags;            Flags específicos del procesador
+    Elf64_Half    e_ehsize;           Tamaño del encabezado ELF
+    Elf64_Half    e_phentsize;        Tamaño de la entrada del encabezado del
+   programa
+   Elf64_Half    e_phnum;            Número de entradas del encabezado
+   del programa
+   Elf64_Half    e_shentsize;        Tamaño de la entrada del
+   encabezado de sección
+   Elf64_Half    e_shnum;            Número de entradas
+   del encabezado de sección
+   Elf64_Half    e_shstrndx;         Índice de la
+   tabla de cadenas del encabezado de sección
+*/
+
 int check_elf64_origin_file(int fd) {
   Elf64_Ehdr header = {0};
   ssize_t i;
