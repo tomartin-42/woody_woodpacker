98a58d622a60f1009fc35188740a9d9e0bebe645
tomartin <tomartin@strudent.42madrid.org>
2024-06-27 21:43:39 +0200

    proble in add my header and change position program header

--- src/set_data.c
+++ src/set_data.c
@@ -25,28 +25,32 @@ Elf64_Addr get_max_add(t_woody *woody) {
 
 void init_my_Pheade(t_woody *woody, void *origin_file) {
   (void)origin_file;
+  Elf64_Off Offset = get_max_add(woody);
+  Offset++;
   woody->my_Pheader = (Elf64_Phdr *)malloc(sizeof(Elf64_Phdr));
   woody->my_Pheader->p_type = PT_LOAD;
-  woody->my_Pheader->p_offset = 0x500000;
-  woody->my_Pheader->p_vaddr = 0x500000;
-  woody->my_Pheader->p_paddr = 0x500000;
+  woody->my_Pheader->p_offset = Offset;
+  woody->my_Pheader->p_vaddr = Offset;
+  woody->my_Pheader->p_paddr = Offset;
   woody->my_Pheader->p_filesz = 0x60;
   woody->my_Pheader->p_memsz = 0x60;
-  woody->my_Pheader->p_flags = 7;
+  woody->my_Pheader->p_flags = PF_X | PF_W | PF_R;
   woody->my_Pheader->p_align = 0x1000;
 }
 
 void mod_origin_header(t_woody *woody, void *origin_file, ssize_t origin_len) {
   // TODO
   (void)woody;
-  Elf64_Ehdr *tmp_header;
-
-  tmp_header = (Elf64_Ehdr *)origin_file;
-  tmp_header->e_phnum++;
-  printf("POFF Before: %ld\n", tmp_header->e_phoff);
-  printf("origin_len: %ld\n", origin_len);
-  tmp_header->e_phoff = origin_len;
-  printf("POFF After: %ld\n", tmp_header->e_phoff);
+  (void)origin_file;
+  (void)origin_len;
+  // Elf64_Ehdr *tmp_header;
+  //
+  // tmp_header = (Elf64_Ehdr *)origin_file;
+  // tmp_header->e_phnum++;
+  // printf("POFF Before: %ld\n", tmp_header->e_phoff);
+  // printf("origin_len: %ld\n", origin_len);
+  // tmp_header->e_phoff = origin_len;
+  // printf("POFF After: %ld\n", tmp_header->e_phoff);
 }
 
 void put_file(t_woody *woody, void *origin_file, ssize_t origin_len) {
@@ -59,7 +63,7 @@ void put_file(t_woody *woody, void *origin_file, ssize_t origin_len) {
   tmp = write(fd, woody->p_header,
               (sizeof(Elf64_Phdr) * (woody->header->e_phnum)));
   printf("[ 2] Write: %d\n", tmp);
-  tmp = write(fd, woody->my_Pheader, sizeof(Elf64_Phdr));
+  // tmp = write(fd, woody->my_Pheader, sizeof(Elf64_Phdr));
   printf("[ 3] Write: %d\n", tmp);
   close(fd);
 }
