8decc1e82091c679241585676c206c943c0c08e8
tomartin <tomartin@strudent.42madrid.org>
2024-07-15 22:14:42 +0200

    first payload work

--- hellorel.asm
+++ hellorel.asm
@@ -1,19 +1,21 @@
-; in `hello.asm`
-
+        bits 64
+        default rel
         global _start
 
-        section .text
-
-_start: mov rdi, 1      ; stdout fd
-        mov rsi, [rel msg]
-        mov rdx, 9      ; 8 chars + newline
+_start: 
+        mov rdi, 1      ; stdout fd
+        lea rsi, [rel msg]
+        mov rdx, 10      ; 9 chars + newline
         mov rax, 1      ; write syscall
         syscall
 
-        xor rdi, rdi    ; return code 0
-        mov rax, 60     ; exit syscall
+        mov     dl, 42         ;petit registre pour enregistrer 42 sans 0x00
+        xor     eax, eax
+        inc     eax
+        mul     dl             ;multiplier 42 * rax (rax = 1), resultat dans rax
+        mov     edi, eax       ;bouger la valeur 42 de rax à rdi
+        xor     eax, eax
+        mov     al, 60         ;on met le syscall exit dans rax
         syscall
         
-        section .data
-
-msg:    db "hi thxrx", 10
+msg    db "..WOODY..", 10
