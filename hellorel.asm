572d2ee68405531bbd3d057a4db57d4106594161
tomartin <tomartin@strudent.42madrid.org>
2024-08-02 07:15:00 +0200

    work fine with hellorel asm, ony print woody and work the origin binary

--- hellorel.asm
+++ hellorel.asm
@@ -3,18 +3,20 @@
                 global _start
 
 _start: 
-                lea rbx, [rel _start] ; get my entry point
+                push rdx
+                lea rbx, [rel _start]
                 mov rdi, 1      ; stdout fd
                 lea rsi, [rel msg]
                 mov rdx, 10      ; 9 chars + newline
                 mov rax, 1      ; write syscall
                 syscall
-                mov rax, [rel distance]
-                sub rbx, rax
+                pop rdx
+                lea rax, [rel distance]
+                sub rbx, [rax]
                 jmp rbx
-                ret
-                ;xor     eax, eax
-                ;mov     al, 60         ;on met le syscall exit dans rax
+                ;ret 
+                ;xor     rax, rax
+                ;mov     al, 60         
                 ;syscall
         
 msg             db "..WOODY..", 10
